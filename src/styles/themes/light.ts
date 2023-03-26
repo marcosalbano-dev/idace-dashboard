83bb0a1e85d7e6df55e80f968d6b0e2f62da5ab0
Marcos Roberto Silva <marcos.telematica@gmail.com>
2023-03-26 19:02:09 -0300

    styles

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+    title: 'light',
+
+    color: {
+        primary: '#DCDCDC',
+        secondary: '#FFFFFF',
+        tertiary: '#F5F5F5',
+
+        white: '#FFF',
+        black: '#000',
+        gray: '#BFBFBF',
+
+        success: '#03BB85',
+        info: '#0B5998',
+        warning: '#FF6961',
+    },
+};
